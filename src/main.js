1c0ae4bb462624a29b728fce895f466912a611c7
Bradley Bossard <bradleybossard@gmail.com>
2025-07-01 20:38:21 -0700

    remove dead code

--- src/main.js
+++ src/main.js
@@ -17,37 +17,6 @@ function initGL() {
 }
 
 function createShaders() {
-  /*
-  var vs = "";
-  vs += "attribute vec4 coords;";
-  vs += "attribute float pointSize;";
-  vs += "void main() {";
-  vs += "  gl_Position = coords;";
-  vs += "  gl_PointSize = pointSize;"; // Set point size
-  vs += "}";
-
-  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-  gl.shaderSource(vertexShader, vs);
-  gl.compileShader(vertexShader);
-
-  var fs =  "";
-  fs += "precision mediump float;";
-  fs += "uniform vec4 color;";
-  fs += "void main() {";
-  fs += "  gl_FragColor = color;"; // Set fragment color
-  fs += "}";
-
-  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-  gl.shaderSource(fragmentShader, fs);
-  gl.compileShader(fragmentShader);
-
-  shaderProgram = gl.createProgram();
-  gl.attachShader(shaderProgram, vertexShader);
-  gl.attachShader(shaderProgram, fragmentShader);
-  gl.linkProgram(shaderProgram);
-  gl.useProgram(shaderProgram);
-  */
-
   var vertexShader = getShader(gl, "shader-vs");
   var fragmentShader = getShader(gl, "shader-fs");
   
@@ -60,20 +29,18 @@ function createShaders() {
 
 
 function createVertices() {
+  vertices = [];
+  for(var i = 0; i < vertexCount; i++) {
+    vertices.push(Math.random() * 2 - 1);
+    vertices.push(Math.random() * 2 - 1);
+  }
 
-  vertices = [
-    -0.9, -0.9, 0.0,
-     0.9, -0.9, 0.0,
-     0.0,  0.9, 0.0,
-  ];
-  
   var buffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
   
 
   var coords = gl.getAttribLocation(shaderProgram, "coords");
-  //gl.vertexAttrib3f(coords, 0.5, 0.5, 0.0);
   gl.vertexAttribPointer(coords, 3, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(coords);
   gl.bindBuffer(gl.ARRAY_BUFFER, null);  
